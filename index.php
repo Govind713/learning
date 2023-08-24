2a1e71404c3201469f9bb3f4c8096ce276e46082
Govind Manoj <govindmanojubuntu@gmail.com>
2023-08-24 12:32:33 +0530

    jtesting

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php include_once("includes/header.php"); ?>
+
+<section id="hmPortfolio" data-aos="fade-up">
+    <div class="container">
+        <h4 class="commonhead textwhite">Specialities</h4>
+        <div class="portfolioTab">
+            <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <button class="nav-link active" id="nav-arthroplasty-tab" data-bs-toggle="tab" data-bs-target="#nav-arthroplasty" type="button" role="tab" aria-controls="nav-arthroplasty" aria-selected="true">Replacement</button>
+                    <button class="nav-link" id="nav-arthroscopy-tab" data-bs-toggle="tab" data-bs-target="#nav-arthroscopy" type="button" role="tab" aria-controls="nav-arthroscopy" aria-selected="false">Restoration</button>
+                    <button class="nav-link" id="nav-trauma-tab" data-bs-toggle="tab" data-bs-target="#nav-trauma" type="button" role="tab" aria-controls="nav-trauma" aria-selected="false">Sports Medicine</button>
+                </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-arthroplasty" role="tabpanel" aria-labelledby="nav-arthroplasty-tab">
+                    <div class="tabContent">
+                        <div class="row">
+                            <div class="col-lg-5 p-0">
+                                <div class="imageLeft">
+                                    <img src="images/tab1.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-7 p-0">
+                                <div class="accordianRight">
+                                    <div class="accordion" id="accordionExample">
+                                        <div class="accordion-item">
+                                            <h5>test1</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="nav-arthroscopy" role="tabpanel" aria-labelledby="nav-arthroscopy-tab">
+                    <div class="tabContent">
+                        <div class="row">
+                            <div class="col-lg-5 p-0">
+                                <div class="imageLeft">
+                                    <img src="images/tab1.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-7 p-0">
+                                <div class="accordianRight">
+                                    <div class="accordion" id="accordionExample1">
+                                        <div class="accordion-item">
+                                            <h5>test2</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="nav-trauma" role="tabpanel" aria-labelledby="nav-trauma-tab">
+                    <div class="tabContent">
+                        <div class="row">
+                            <div class="col-lg-5 p-0">
+                                <div class="imageLeft">
+                                    <img src="images/tab1.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-7 p-0">
+                                <div class="accordianRight">
+                                    <div class="accordion" id="accordionExample3">
+                                        <div class="accordion-item">
+                                            <h5>test3</h5>
+                                        </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include_once("includes/footer.php"); ?>
